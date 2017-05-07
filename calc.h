48ba336df90edf1fc919600d6c03adc8a09c50e1
Robert Garza <robstudent1988@gmail.com>
2017-05-07 15:20:48 -0700

    Added get_val, returns unsigned char. Changed calc.get_val to return unsigned char

--- calc.h
+++ calc.h
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//string either number or single letter
+//if single_letter -> refer to class, get value for letter
+//convert char into an integer
+
+unsigned char get_val(string value) {
+    if(isdigit(value[0])) {
+        return stoi(value);
+    } else {
+        unsigned char placeHolder = value[0];
+        return table.at(placeHolder)->get_val();
+    }
+};
+
 class calc{
     protected:
         unsigned char val;
@@ -17,13 +30,12 @@ class calc{
             val = a;
             used = true;
         }
-        int get_val () {
+        unsigned char get_val () {
             unsigned char temp = val;
             val++;
             if (val == 0){
                 cout << "bad input from class" << endl;
             }
             return temp;
-
-    }
+        }
 };
