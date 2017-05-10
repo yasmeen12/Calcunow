120cc4f442bef0349551b5beec15627085433b23
yasmeen12 <yasmeenmaklani12@gmail.com>
2017-05-10 16:05:28 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -9,7 +9,8 @@ int main() {
     //x + 43
     for(int i = 0; i < 26; i++) table.push_back(new calc);
     string input = "";
-    while (cin) {
+   int looping = 0; 
+    while (true) {
         vector <string> calcInputs;
         auto c = cin.peek();
         if (c == EOF) break;
@@ -23,7 +24,15 @@ int main() {
             calcInputs.push_back(word);
         }
         try{
-            if( calcInputs.at(0) == "LET") set_value(calcInputs.at(1)[0], stoi(calcInputs.at(3)));
+           if( looping > 0) {
+                  for (int i = 0; i < looping; i++) {
+                      cout << math(calcInputs) << endl;
+                  }
+              looping = 0;
+              }
+              else if( calcInputs.at(0) == "LET") set_value(calcInputs.at(1)[0], stoi(calcInputs.at(3)));
+              else if(calcInputs.at(0) == "QUIT" || calcInputs.at(0) == "quit") break;
+              else if(calcInputs.at(0) == "LOOP") looping = stoi( calcInputs.at(1));
             else cout <<  math(calcInputs) << endl;
         } catch (exception &e) {
                 cout << "INVALID INPUT!\n";
