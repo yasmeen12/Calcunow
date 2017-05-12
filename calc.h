e14905b4c534180ea165e264d96f7b6db6dd8669
Robert Garza <robstudent1988@gmail.com>
2017-05-11 18:48:42 -0700

    Update calc.h
    Added more comments to explain throw conditions and calc class description.

--- calc.h
+++ calc.h
@@ -4,35 +4,46 @@
 #include <cmath>
 using namespace std;
 
+//calc class is used to store variables ascii characters
+//keeps track of variables being assigned int value
+
 class calc{
     protected:
         unsigned char val;
         bool used;
 
     public:
+        //No argument constructor
+        //Class will be inserted 26 times into table
         calc() {
             val = 0;
+            //Check if val a-z are set.
             used = false;
         }
-
-        bool get_used () {return used;}
+    
+        bool get_used () {return used; }
+    
         void set_val (int a) {
             val = a;
             used = true;
         }
+    
         unsigned char get_val () {
             unsigned char temp = val;
+            //val increments. If 0 it has wrapped around, throw error
             val++;
             if (val == 0) throw runtime_error("Bad!");
             return temp;
         }
 };
 
+//vector stores pointers to 26 calc class objects
 vector<calc*> table;
 
 void set_value(char var, int val) {
     int temp = var - 'a';
     //Check if value already set
+    //If value is is outside 255 ascii characters, throw an error
     if(table.at(temp)->get_used() || val < 0 || val > 255) throw runtime_error("Bad!");
     table.at(temp)->set_val(val);
 };
@@ -42,7 +53,7 @@ unsigned char get_val(string value) {
         return stoi(value);
     } else {
         unsigned char placeHolder = value[0]- 'a';
-        //check if value has not been set
+        //check if value has not been set. If not set throw error
         if(!table.at(placeHolder)->get_used()) throw runtime_error("Bad!");
         return table.at(placeHolder)->get_val();
     }
@@ -70,6 +81,7 @@ int math(vector<string> input) {
             value = value * second_var;
         }
         if (operand == '/') {
+            //If attempting to divide by 0, throw error
             if(second_var == 0) throw runtime_error("Bad!");
             value = value / second_var;
         }
@@ -77,6 +89,7 @@ int math(vector<string> input) {
             value = value % second_var;
         }
         if (operand == '^') {
+            //0 to the power of 0, or variable to the power of 0, throw error
             if(value == 0 || second_var == 0)
                 throw runtime_error("Invalid Input!");
             else value = pow(value,second_var);
